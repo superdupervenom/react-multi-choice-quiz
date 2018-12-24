e527cb89c3650dbbb96550fdfe99cdd16e88f3f3
superdupervenom <brysonhilton@gmail.com>
2018-12-23 22:01:13 -0800

    fixed app

--- src/components/Result.js
+++ src/components/Result.js
@@ -14,12 +14,23 @@ function Result(props) {
       transitionAppear
       transitionAppearTimeout={500}
     >
-      <div class="video-container">
-        <h1>
-          Merry Christmas Ludim. Thank you for all the joy and love you have brought into my life!
-        </h1>
-        <iframe width="853" height="480" src="https://www.youtube.com/embed/OM-lp2FAJGQ?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+  
+    <div class="row">
+      <div class="col m6 offset-m3">
+        <div class="card">
+          <div class="card-image">
+            <div class="video-container"> 
+              <iframe width="853" height="480" src="https://www.youtube.com/embed/Bweau5RYAe8?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe> 
+            </div>
+          </div>
+          <div class="card-content">
+            <span class="card-title"> Merry Christmas Ludim. Thank you for all the joy and love you have brought into my life! Love, Bryson </span>
+            
+          </div>
+        </div>
       </div>
+    </div>
+
     </CSSTransitionGroup>
   );
 }
